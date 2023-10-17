675a82d001c1a98d57f09ab7eab826b7cb35fab9
Karl F <144461089+phanther1@users.noreply.github.com>
2023-10-17 18:47:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const DOMSelectors = {
+    input: document.querySelector("#input"),
+    liquid: document.querySelector(".liquid"),
+    color: document.querySelector(".liquid"),
     button: document.getElementById("click me"),
+
     text: document.querySelectorAll("#select"),
     input: document.querySelector(`#input`),
 }
 
 function add() {
-
+    DOMSelectors.button.insertAdjacentElement("#input")
+    {DOMSelectors.liquid.onClick} {DOMSelectors.color.onClick}
 } 
 
 document.body.style.backgroundColor = "purple";
