5408796c3d3b0d15fee4666b82ebef8611e7292b
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-10-25 10:15:22 -0400

    df

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-
-
 const DOMSelectors = {
     input: document.querySelector("#input"),
     liquid: document.querySelector(".liquid"),
     color: document.querySelector(".liquid"),
-    button: document.getElementById("click me"),
-
+    button: document.getElementById("SubmitBtn"),
     text: document.querySelectorAll("#select"),
     input: document.querySelector(`#input`),
 }
 
+document.getElementById("SubmitBtn").addEventListener("click", function(event){
+    event.preventDefault();
+}),
 
 
 function add() {
-    DOMSelectors.button.insertAdjacentElement("#input")
+    DOMSelectors.button.insertAdjacentHTML("#input")
     {DOMSelectors.liquid.onClick} {DOMSelectors.color.onClick}
 } 
 
-function remove() {
+
+function removeInput() {
     DOMSelectors.button
 }
 
+const btnRemove = document.getElementById("remove");
+btnRemove.addEventListener("")
 
-
+document.getElementById("button").addEventListener("click")event)
 document.body.style.backgroundColor = "purple";
 
 console.log (DOMSelectors);
