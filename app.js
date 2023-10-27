a35afd992e594396b5e4076715f6e6c1e2b5f78a
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-10-27 10:13:50 -0400

    f

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const DOMSelectors = {
     input: document.querySelectorAll("input"),
     liquid: document.getElementById("liquid"),
     color: document.getElementById("color"),
-    link: document.getElementById("link"),
+    link: document.getElementById("image"),
     button: document.getElementById("btn"),
     cards: document.querySelector("gallery"),
 }
@@ -12,20 +12,19 @@ const DOMSelectors = {
 function add() {
     DOMSelectors.insertAdjacentHTML("beforeend",
     `<div class="cards">
-    <h2>${DOMSelectors.liquid.value}</h2>
-    <a href=${DOMSelectors.link.value}>click here</a>
-    <h3>${DOMSelectors.color.value}</h3>
+        <h2>${DOMSelectors.liquid.value}</h2>
 
-    <button class = "Remove">Remove</button>
-  </div>`);
+        <h3>${DOMSelectors.color.value}</h3>
+        <button class="clear">Remove</button>
+    </div>`);
     
 } 
 
 function clear() {
     const clear = document.cards.querySelectorAll(".clear");
     clear.forEach((clicked) => {
-        clicked.addEventListener("click", function(clicked) {
-        clicked.target.parentElement.clear(); 
+        clicked.addEventListener('click', function(clicked) {
+        clicked.target.parentElement.clear();
         });
     });
 }
@@ -42,9 +41,10 @@ DOMSelectors.form.addEventListener("submit", function(event) {
     add();
     clear();
     clearInput();
+
 }
 )
 
-document.body.style.backgroundColor = "purple";
+
 
 /* document.getElementById("button").addEventListener("click"); */
